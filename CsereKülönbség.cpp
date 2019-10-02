69f2019e5d7b11fd7ac001343cc7c1c51fce3bc9
KBence99 <55990106+KBence99@users.noreply.github.com>
2019-10-02 19:43:44 +0200

    Add files via upload

---
+++ CsereKülönbség.cpp
@@ -0,0 +1,13 @@
+#include "std_lib_facilities.h"
+int main()
+{
+	int a=5;
+	int b=7;
+	cout<<"A értéke: "<<a<<"\nB értéke: "<<b<<"\n";
+	cout<<"\n";
+	a = a+b;
+	b = a-b;
+	a = a-b;
+	cout<<"A értéke: "<<a<<"\nB értéke: "<<b<<"\n";
+	cout<<"\n";	
+}
